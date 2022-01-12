504f9246336277fe14ff11bfeabd61fcdb5d0ff9
Canberk Baykal <38884482+johnberg1@users.noreply.github.com>
2022-01-12 22:56:41 +0300

    fix interpolation code

--- models/psp_interpolate.py
+++ models/psp_interpolate.py
@@ -11,8 +11,8 @@ import math
 from configs.paths_config import model_paths
 from models.encoders import psp_encoders
 from models.encoders import psp_encoders_adain
-from models.stylegan2.model_ada4_all import Generator, Discriminator
-from models.stylegan2.model import Generator as Generator2
+from models.stylegan2.model import Generator
+
 
 class pSp(nn.Module):
 
@@ -23,8 +23,6 @@ class pSp(nn.Module):
 		# Define architecture
 		self.encoder = self.set_encoder()
 		self.decoder = Generator(self.opts.output_size, 512, 8)
-		self.decoder_fixed = Generator2(self.opts.output_size, 512, 8)
-		self.discriminator = Discriminator(self.opts.output_size)
 		self.face_pool = torch.nn.AdaptiveAvgPool2d((256, 256))
 		# Load weights if needed
 		self.load_weights()
@@ -37,12 +35,7 @@ class pSp(nn.Module):
 			print(f'Loading SAM from checkpoint: {self.opts.checkpoint_path}')
 			ckpt = torch.load(self.opts.checkpoint_path, map_location='cpu')
 			self.encoder.load_state_dict(self.__get_keys(ckpt, 'encoder'), strict=False)
-			self.decoder.load_state_dict(self.__get_keys(ckpt, 'decoder'), strict=False) # CHANGED
-			self.decoder_fixed.load_state_dict(self.__get_keys(ckpt, 'decoder'), strict=True) # CHANGED
-			ckpt_d = torch.load(self.opts.stylegan_weights)
-			self.discriminator.load_state_dict(ckpt_d['d'], strict=True)
-			for param in self.decoder_fixed.parameters():
-				param.requires_grad = False
+			self.decoder.load_state_dict(self.__get_keys(ckpt, 'decoder'), strict=True)
 			if self.opts.start_from_encoded_w_plus:
 				self.pretrained_encoder = self.__get_pretrained_psp_encoder()
 				self.pretrained_encoder.load_state_dict(self.__get_keys(ckpt, 'pretrained_encoder'), strict=True)
@@ -59,7 +52,7 @@ class pSp(nn.Module):
 			self.encoder.load_state_dict(encoder_ckpt, strict=False)
 			print(f'Loading decoder weights from pretrained path: {self.opts.stylegan_weights}')
 			ckpt = torch.load(self.opts.stylegan_weights)
-			self.decoder.load_state_dict(ckpt['g_ema'], strict=False) # CHANGED
+			self.decoder.load_state_dict(ckpt['g_ema'], strict=True)
 			self.__load_latent_avg(ckpt, repeat=self.n_styles)
 			if self.opts.start_from_encoded_w_plus:
 				self.pretrained_encoder = self.__load_pretrained_psp_encoder()
@@ -96,23 +89,15 @@ class pSp(nn.Module):
 		images, result_latent = self.decoder([codes],
 											 input_is_latent=input_is_latent,
 											 randomize_noise=randomize_noise,
-											 return_latents=return_latents,
-                       txt_embed=txt_embed)
-        
-		fake_pred = self.discriminator(images)
-		images_fixed, _ = self.decoder_fixed([codes],
-											 input_is_latent=input_is_latent,
-											 randomize_noise=randomize_noise,
 											 return_latents=return_latents)
 
 		if resize:
 			images = self.face_pool(images)
-			images_fixed = self.face_pool(images_fixed)
 
 		if return_latents:
-			return images, result_latent, images_fixed, fake_pred, codes, psp_out
+			return images, result_latent, codes, psp_out
 		else:
-			return images, images_fixed, fake_pred, codes, psp_out
+			return images, codes, psp_out
 
 	def set_opts(self, opts):
 		self.opts = opts
