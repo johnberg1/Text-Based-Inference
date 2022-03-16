24d33c678ada57ec2e27ab80768ecbcf37c63f88
Canberk Baykal <38884482+johnberg1@users.noreply.github.com>
2022-03-16 11:27:13 +0300

    s space interpolation

---
+++ models/psp_s_interpolate.py
@@ -0,0 +1,166 @@
+"""
+This file defines the core research contribution
+"""
+import copy
+from argparse import Namespace
+
+import torch
+from torch import nn
+import math
+
+from configs.paths_config import model_paths
+from models.encoders import psp_encoders_S
+from models.encoders import psp_encoders_S_adain
+from models.stylegan2.model_s import Generator
+from operator import add
+
+class pSp(nn.Module):
+
+	def __init__(self, opts):
+		super(pSp, self).__init__()
+		self.set_opts(opts)
+		self.n_styles = int(math.log(self.opts.output_size, 2)) * 2 - 2
+		# Define architecture
+		self.encoder = self.set_encoder()
+		self.decoder = Generator(self.opts.output_size, 512, 8)
+		self.face_pool = torch.nn.AdaptiveAvgPool2d((256, 256))
+		# Load weights if needed
+		self.load_weights()
+
+	def set_encoder(self):
+		return psp_encoders_S_adain.GradualStyleEncoder(50, 'ir_se', self.n_styles, self.opts)
+
+	def load_weights(self):
+		if self.opts.checkpoint_path is not None:
+			print(f'Loading SAM from checkpoint: {self.opts.checkpoint_path}')
+			ckpt = torch.load(self.opts.checkpoint_path, map_location='cpu')
+			self.encoder.load_state_dict(self.__get_keys(ckpt, 'encoder'), strict=False)
+			self.decoder.load_state_dict(self.__get_keys(ckpt, 'decoder'), strict=True)
+			if self.opts.start_from_encoded_w_plus:
+				self.pretrained_encoder = self.__get_pretrained_psp_encoder()
+				self.pretrained_encoder.load_state_dict(self.__get_keys(ckpt, 'pretrained_encoder'), strict=True)
+			self.__load_latent_avg(ckpt)
+		else:
+			print('Loading encoders weights from irse50!')
+			encoder_ckpt = torch.load(model_paths['ir_se50'])
+			# Transfer the RGB input of the irse50 network to the first 3 input channels of SAM's encoder
+			if self.opts.input_nc != 3:
+				shape = encoder_ckpt['input_layer.0.weight'].shape
+				altered_input_layer = torch.randn(shape[0], self.opts.input_nc, shape[2], shape[3], dtype=torch.float32)
+				altered_input_layer[:, :3, :, :] = encoder_ckpt['input_layer.0.weight']
+				encoder_ckpt['input_layer.0.weight'] = altered_input_layer
+			self.encoder.load_state_dict(encoder_ckpt, strict=False)
+			print(f'Loading decoder weights from pretrained path: {self.opts.stylegan_weights}')
+			ckpt = torch.load(self.opts.stylegan_weights)
+			with torch.no_grad():
+				# nn.init.normal_(self.encoder.affine_transforms[0].weight, std=1e-2)
+				# nn.init.normal_(self.encoder.affine_transforms[1].weight, std=1e-2)
+				# self.encoder.affine_transforms[0].bias.data.fill_(0.0)
+				# self.encoder.affine_transforms[1].bias.data.fill_(0.0)
+				self.encoder.affine_transforms[0].weight.copy_(ckpt['g_ema']['conv1.conv.modulation.weight'])
+				self.encoder.affine_transforms[0].bias.copy_(ckpt['g_ema']['conv1.conv.modulation.bias'])
+				self.encoder.affine_transforms[1].weight.copy_(ckpt['g_ema']['to_rgb1.conv.modulation.weight'])
+				self.encoder.affine_transforms[1].bias.copy_(ckpt['g_ema']['to_rgb1.conv.modulation.bias'])
+			q, p = 0, 0
+			for j in range(2,26,3):
+				with torch.no_grad():
+					# nn.init.normal_(self.encoder.affine_transforms[j].weight, std=1e-2)
+					# nn.init.normal_(self.encoder.affine_transforms[j+1].weight, std=1e-2)
+					# nn.init.normal_(self.encoder.affine_transforms[j+2].weight, std=1e-2)
+					# self.encoder.affine_transforms[j].bias.data.fill_(0.0)
+					# self.encoder.affine_transforms[j+1].bias.data.fill_(0.0)
+					# self.encoder.affine_transforms[j+2].bias.data.fill_(0.0)
+					self.encoder.affine_transforms[j].weight.copy_(ckpt['g_ema']['convs.{}.conv.modulation.weight'.format(q)])
+					self.encoder.affine_transforms[j].bias.copy_(ckpt['g_ema']['convs.{}.conv.modulation.bias'.format(q)])
+					self.encoder.affine_transforms[j+1].weight.copy_(ckpt['g_ema']['convs.{}.conv.modulation.weight'.format(q+1)])
+					self.encoder.affine_transforms[j+1].bias.copy_(ckpt['g_ema']['convs.{}.conv.modulation.bias'.format(q+1)])
+					self.encoder.affine_transforms[j+2].weight.copy_(ckpt['g_ema']['to_rgbs.{}.conv.modulation.weight'.format(p)])
+					self.encoder.affine_transforms[j+2].bias.copy_(ckpt['g_ema']['to_rgbs.{}.conv.modulation.bias'.format(p)])
+					p += 1
+					q += 2
+			self.decoder.load_state_dict(ckpt['g_ema'], strict=False)
+			self.__load_latent_avg(ckpt, repeat=self.n_styles)
+			if self.opts.start_from_encoded_w_plus:
+				self.pretrained_encoder = self.__load_pretrained_psp_encoder()
+				self.pretrained_encoder.eval()
+
+	def forward(self, x, txt_embed, resize=True, latent_mask=None, input_code=False, randomize_noise=True,
+				inject_latent=None, return_latents=False, alpha=None, input_is_full=False):
+		if input_code:
+			codes = x
+		else:
+			codes = self.encoder(x, txt_embed)
+			residuals = codes
+			# normalize with respect to the center of an average face
+			if self.opts.start_from_latent_avg:
+				codes = codes #+ self.latent_avg
+			# normalize with respect to the latent of the encoded image of pretrained pSp encoder
+			elif self.opts.start_from_encoded_w_plus:
+				with torch.no_grad():
+					encoded_latents = self.pretrained_encoder(x)
+					encoded_latents = encoded_latents #+ self.latent_avg
+					psp_out = encoded_latents
+				codes = list(map(add,codes,encoded_latents))
+
+		if latent_mask is not None:
+			for i in latent_mask:
+				if inject_latent is not None:
+					if alpha is not None:
+						codes[:, i] = alpha * inject_latent[:, i] + (1 - alpha) * codes[:, i]
+					else:
+						codes[:, i] = inject_latent[:, i]
+				else:
+					codes[:, i] = 0
+
+		input_is_latent = (not input_code) or (input_is_full)
+		images, result_latent = self.decoder([codes],
+											 input_is_latent=input_is_latent,
+											 randomize_noise=randomize_noise,
+											 return_latents=return_latents)
+        
+		inversions, _ = self.decoder([encoded_latents],
+											 input_is_latent=input_is_latent,
+											 randomize_noise=randomize_noise,
+											 return_latents=False)
+
+		if resize:
+			images = self.face_pool(images)
+			inversions = self.face_pool(inversions)
+
+		if return_latents:
+			return images, result_latent, inversions, residuals, codes, psp_out
+		else:
+			return images, inversions, residuals, codes, psp_out
+
+	def set_opts(self, opts):
+		self.opts = opts
+
+	def __load_latent_avg(self, ckpt, repeat=None):
+		if 'latent_avg' in ckpt:
+			self.latent_avg = ckpt['latent_avg'].to(self.opts.device)
+			if repeat is not None:
+				self.latent_avg = self.latent_avg.repeat(repeat, 1)
+		else:
+			self.latent_avg = None
+
+	def __get_pretrained_psp_encoder(self):
+		opts_encoder = vars(copy.deepcopy(self.opts))
+		opts_encoder['input_nc'] = 3
+		opts_encoder = Namespace(**opts_encoder)
+		encoder = psp_encoders_S.GradualStyleEncoder(50, 'ir_se', self.n_styles, opts_encoder)
+		return encoder
+
+	def __load_pretrained_psp_encoder(self):
+		print(f'Loading pSp encoder from checkpoint: {self.opts.pretrained_psp_path}')
+		ckpt = torch.load(self.opts.pretrained_psp_path, map_location='cpu')
+		encoder_ckpt = self.__get_keys(ckpt, name='encoder')
+		encoder = self.__get_pretrained_psp_encoder()
+		encoder.load_state_dict(encoder_ckpt, strict=False)
+		return encoder
+
+	@staticmethod
+	def __get_keys(d, name):
+		if 'state_dict' in d:
+			d = d['state_dict']
+		d_filt = {k[len(name) + 1:]: v for k, v in d.items() if k[:len(name)] == name}
+		return d_filt
